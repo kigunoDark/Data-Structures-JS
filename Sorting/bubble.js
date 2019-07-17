f3bb16df8e52f8de853ad2439b925b658789aa61
foxbuka <krushenitskii@gmail.com>
2019-07-17 17:00:47 +0300

    selection sort

--- Sorting/bubble.js
+++ Sorting/bubble.js
@@ -22,6 +22,3 @@ function buble(arr)
   }
        return arr;
 }
-  
-  return arr;
-}
---
+++ Sorting/selection.js
@@ -0,0 +1,17 @@
+function buble(arr)
+{
+  
+    
+  for(let i = 0; i<arr.length; i++)
+  {
+    let lowest = i;
+    for(let j = i+1; j <arr.length; j++)
+    {
+      if(arr[j] < arr[lowest]){
+        lowest = j;
+      }
+      [arr[i],arr[lowest]] = [arr[lowest], arr[i]];
+    }  
+  }
+  return arr;
+}
