e87e429185f987ed7fdb66a6e5ade17e299f6747
foxbuka <krushenitskii@gmail.com>
2019-07-17 16:07:52 +0300

    b sort optimization

--- Sorting/bubble.js
+++ Sorting/bubble.js
@@ -1,20 +1,27 @@
 function buble(arr)
 {
+   let noSwaps = true;
+
   for(let i = arr.length; i > 0; i--)
   {
      
     for( let j = 0; j < i-1; j++)
     {
+      console.log(arr, arr[j], arr[j+1]);
      
       if(arr[j] > arr[j+1])
       {
         let a = arr[j];
         arr[j] = arr[j+1];
         arr[j+1] = a; 
+        noSwaps = false;
       }
+
     }
-   
+     if(noSwaps) break;
   }
+       return arr;
+}
   
   return arr;
 }
